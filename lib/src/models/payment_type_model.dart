439b021fcb10f1745fc5ae5437b0f0e4ee37cd8e
FelipeSBG14 <borges.felipe.felipe14@gmail.com>
2023-05-12 15:37:04 -0400

    erro aula 3

--- lib/src/models/payment_type_model.dart
+++ lib/src/models/payment_type_model.dart
@@ -6,12 +6,12 @@ class PaymentTypeModel {
   final int? id;
   final String name;
   final String acronym;
-  final bool enable;
+  final bool enabled;
   PaymentTypeModel({
     this.id,
     required this.name,
     required this.acronym,
-    required this.enable,
+    required this.enabled,
   });
 
   Map<String, dynamic> toMap() {
@@ -19,7 +19,7 @@ class PaymentTypeModel {
       'id': id,
       'name': name,
       'acronym': acronym,
-      'enable': enable,
+      'enabled': enabled,
     };
   }
 
@@ -28,7 +28,7 @@ class PaymentTypeModel {
       id: map['id'] != null ? map['id'] as int : null,
       name: map['name'] as String,
       acronym: map['acronym'] as String,
-      enable: map['enable'] as bool,
+      enabled: map['enabled'] as bool,
     );
   }
 
