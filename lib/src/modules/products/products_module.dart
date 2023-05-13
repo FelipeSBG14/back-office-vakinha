988456a2ea93f5ef26cf2471ab40c327c7b5b11d
FelipeSBG14 <borges.felipe.felipe14@gmail.com>
2023-05-13 15:08:17 -0400

    erro aula 4

---
+++ lib/src/modules/products/products_module.dart
@@ -0,0 +1,17 @@
+import 'package:flutter_modular/flutter_modular.dart';
+
+import 'home/products_page.dart';
+
+class ProductsModule extends Module {
+  @override
+  List<Bind> get binds => [];
+
+  @override
+  List<ModularRoute> get routes => [
+        ChildRoute(
+          '/',
+          child: (context, args) => ProductsPage(),
+        ),
+        // ChildRoute('/detail', child: (context, args) => ProductsPage(),),
+      ];
+}
